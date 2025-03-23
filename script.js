19cd45e01a98f04c86e387eed7b8e091e17b0029
Faojulazim <faojulazim13@gmail.com>
2025-03-24 00:19:15 +0600

    new

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const tabs = document.querySelectorAll("[data-tabs]");
 const tabClicker = document.querySelectorAll("[data-tabClicker]");
 const dropdown = document.querySelectorAll("[data-dropdown]");
+const hiddenDiv = document.querySelector("#hiddenDiv");
+const hamburgerIcon = document.querySelector("#hamburger");
+const closeIcon = document.querySelector("#close");
+const errorPara = document.querySelector("#errorPara");
+const submitBtn = document.querySelector("#submitBtn");
+const form = document.querySelector("form");
+const changeStyles = document.querySelector("#changeStyles");
+const input = document.querySelector("input");
+
+hamburgerIcon.addEventListener("click", (e) => {
+  hiddenDiv.classList.toggle("top-[-200%]");
+});
+closeIcon.addEventListener("click", (e) => {
+  hiddenDiv.classList.toggle("top-[-200%]");
+});
 
 dropdown.forEach((items) => {
   items.addEventListener("click", (e) => {
@@ -49,8 +64,32 @@ function receiveItemsID(itemsID) {
   tabs.forEach((elems) => {
     if (itemsID === elems.id) {
       elems.classList.remove("hidden");
+      elems.classList.remove("lg:hidden");
     } else {
       elems.classList.add("hidden");
+      elems.classList.add("lg:hidden");
     }
   });
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.value)) {
+      errorPara.classList.remove("hidden");
+      errorPara.innerText = "Email Sent!";
+      changeStyles.classList.add("p-[3px]", "bg-green-500");
+      changeStyles.classList.remove("bg-softRed");
+      input.value = "";
+      setTimeout(() => {
+        errorPara.classList.add("hidden");
+        changeStyles.classList.remove("bg-softRed", "bg-green-500", "p-[3px]");
+        errorPara.innerText = "Whoops, make sure it's an email";
+      }, 2500);
+    }
+  } else {
+    errorPara.innerText = "Whoops, make sure it's an email";
+    errorPara.classList.remove("hidden");
+    changeStyles.classList.add("p-[3px]", "bg-softRed");
+  }
+});
